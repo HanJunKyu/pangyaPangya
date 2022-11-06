eba90d628c199d1b849108051b4beec6a6cd1723
han <wnsrbod@naver.com>
2022-11-07 00:19:30 +0900

    커밋 테스트2

---
+++ src/main/java/com/example/pangyapangya/beans/vo/TestingReviewVO.java
@@ -0,0 +1,16 @@
+package com.example.pangyapangya.beans.vo;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class TestingReviewVO {
+    private Long rno;
+    private String title;
+    private String content;
+    private String img;
+    private String userId;
+    private int rating;
+    private Long tno;
+}
